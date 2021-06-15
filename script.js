00e42891327d190aa9e1003f0f70a8e65000db98
oscarschenk <79904169+oscarschenk@users.noreply.github.com>
2021-06-15 14:36:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+function menuToggle() {
+    let toggle = document.getElementById("menu");
+    toggle.classList.add("display");
+    body.classList.toggle("reset");
+    body.classList.remove("rood");
+    body.classList.remove("groen")
+    body.classList.remove("blauw")
+    body.classList.remove("roze")
+    body.classList.remove("paars")
+    kleurText.innerHTML = "Ik ben WIT";
+  }
+
+function colorMenuDisappear() {
+    colorMenuToggle.classList.remove("display");
+}
+
+let burgerToggle = document.getElementById("burgerToggle")
+let colorMenuToggle = document.getElementById("menu")
+
+burgerToggle.addEventListener("mouseover", menuToggle);
+colorMenuToggle.addEventListener("mouseleave", colorMenuDisappear);
+
+
+let kleuren = document.querySelectorAll('input[name="kleuren"]');
+
+let kleurenArray = Array.from(kleuren);
+
+
+let rood = kleurenArray[0];
+let groen = kleurenArray[1];
+let blauw = kleurenArray[2];
+let roze = kleurenArray[3];
+let paars = kleurenArray[4];
+let body = document.getElementById("body");
+let kleurText = document.querySelector(".kleurText");
+
+let maakRood = function (){
+    body.classList.add("rood");
+    body.classList.remove("reset")
+    body.classList.remove("groen")
+    body.classList.remove("blauw")
+    body.classList.remove("roze")
+    body.classList.remove("paars")
+    kleurText.innerHTML = "Ik ben ROOD";
+}
+
+let maakGroen = function (){
+    body.classList.add("groen");
+    body.classList.remove("reset")
+    body.classList.remove("rood")
+    body.classList.remove("blauw")
+    body.classList.remove("roze")
+    body.classList.remove("paars")
+    kleurText.innerHTML = "Ik ben GROEN";
+}
+
+let maakBlauw = function (){
+    body.classList.add("blauw");
+    body.classList.remove("reset")
+    body.classList.remove("groen")
+    body.classList.remove("rood")
+    body.classList.remove("roze")
+    body.classList.remove("paars")
+    kleurText.innerHTML = "Ik ben BLAUW";
+}
+
+let maakRoze = function (){
+    body.classList.add("roze");
+    body.classList.remove("reset")
+    body.classList.remove("groen")
+    body.classList.remove("blauw")
+    body.classList.remove("rood")
+    body.classList.remove("paars")
+    kleurText.innerHTML = "Ik ben ROZE";
+}
+
+let maakPaars = function (){
+    body.classList.add("paars");
+    body.classList.remove("reset")
+    body.classList.remove("groen")
+    body.classList.remove("blauw")
+    body.classList.remove("roze")
+    body.classList.remove("rood")
+    kleurText.innerHTML = "Ik ben PAARS";
+}
+
+rood.addEventListener("click", maakRood);
+groen.addEventListener("click", maakGroen);
+blauw.addEventListener("click", maakBlauw);
+roze.addEventListener("click", maakRoze);
+paars.addEventListener("click", maakPaars);
+
+const keyPress = function (event) {
+    const x = event.keyCode;
+
+    if (x == 49) {
+        maakRood();
+    }
+
+    if (x == 50) {
+        maakBlauw();
+    }
+
+    if (x == 51) {
+        maakGroen();
+    }
+
+    if (x == 52) {
+        maakRoze();
+    }
+
+    if (x == 53) {
+        maakPaars();
+    }
+}
+
+document.addEventListener('keypress', keyPress);
